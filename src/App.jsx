058dbc5f297f1dbfd51b49b292037caea27ece43
Duc Hieu Ngo <k2358612@kingston.ac.uk>
2025-01-21 15:52:11 +0000

    changed the import for ModuleView on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./components/views/Home.jsx";
 import Modules from "./components/views/Modules.jsx";
 import PageNotFound from "./components/views/PageNotFound.jsx";
 import Availability from "./components/views/Availability.jsx";
-import ModuleView from "./Components/views/ModuleView.jsx";
+import ModuleView from "./ChampionComponents/views/ModuleView.jsx";
 
 function App() {
   const loggedInUser = "Test Mentor";
